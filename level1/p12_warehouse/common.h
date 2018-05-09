238b9485424bb4e7de04b6365d105e45f3744126
renzhenhua <37101383+renzhenhua@users.noreply.github.com>
2018-05-09 22:39:38 +0800

    Create common.h

---
+++ level1/p12_warehouse/common.h
@@ -0,0 +1,13 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include"tools.h"
+
+#define FILENAME "warehouse.data" //保存货物信息的文件名称(含路径) 
+
+#define MAX_WARE_ID 99         //货物型号最大值
+#define MAX_WARE_NAME 10       //货物名称最大值
+#define MAX_WARE_NUMBER 100    //货物数量最大值 
+#define MAX_WARE_PRICE 1500    //货物价格最大值 
+
+#endif
